72375cf66d530c972c7b5e4ebf86cc667d19d8b3
guilherme93 <guilherme_753@msn.com>
2016-10-07 04:17:13 +0100

    Add files via upload

--- SolF1.lisp
+++ SolF1.lisp
@@ -11,19 +11,52 @@
 ;(load "datastructures.fas")
 ;(load "auxfuncs.fas")
 
+
+(load "datastructures.lisp")
+(load "auxfuncs.lisp")
+
 (defun isObstaclep (pos track) 
   "check if there is an obstacle at position pos of the track"
-  t)
+	(if(eq(nth (car(cdr pos)) (nth (car pos)(Track-env track))) nil) 
+		(return-from isObstaclep T))
+	nil
+)
 
 (defun isGoalp (st) 
   "check if st is a goal state"
-  t)
+	(loop for e in (Track-endpositions (State-track st))
+		do (if(equal  e (State-pos st)) (return-from isGoalp T))
+	)
+  nil
+)
+
+;dada duas listas devolve a  lista com a soma das duas tem de ter tamanho 2
+(defun addList (list1 list2)
+	(list (+ (car list1)  (car list2)) (+ (car(cdr list1)) (car (cdr list2))))
+)
 
 (defun nextState (st act)
   "generate the nextState after state st and action act"
-  (make-STATE :POS '(3 16)
-	      :VEL '(1 3)
-	      :ACTION act
-	      :COST -100))
+	(let((newVelocity  (addList (State-vel st) act)) (newPos )(newState))
+	(setf newPos (addList  newVelocity  (State-pos st)))
+	(setf newState
+		(make-STATE :POS newPos
+			  :VEL newVelocity
+			  :ACTION act
+			  :TRACK (State-track st)
+			  :COST 0
+		)
+	)
+	(cond ((isGoalp newState)
+       (setf (State-cost newState) -100)
+      ) ;_ end of the first then condition
+       ((isObstaclep newPos (State-track st)) 
+		(setf (State-cost newState) 20)
+	   ) ;_ end of the second then condition  
+	  (t (setf (State-cost newState) 1))  ;else,
+	) ;_ end of cond statement
+	newState			
+	)
+)
 
 
